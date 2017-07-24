f5d40b2921fdb818aba7cd4def67b17f1e1e58e9
akomawar <komawarankit67@gmail.com>
2017-07-23 21:39:14 -0700

    Functions in Python

---
+++ function.py
@@ -0,0 +1,10 @@
+def addition():
+    n1=int(input("Enter 1st no.: "))
+    n2=int(input("Enter 2nd no.: "))
+
+    print("Addition of 1st and 2nd is: ", n1+n2)
+    
+def subtraction():
+    n1=int(input("Enter 1st no.: "))
+    n2=int(input("Enter 2nd no.: "))
+    print("Subtraction of 1st and 2nd is: ",n1-n2)
