dc8d97aa9564bdf0c8ef3c55d452520b2d4cbb69
Ankit Komawar <komawarankit67@gmail.com>
2017-11-28 12:14:14 -0800

    textwrap

---
+++ Text Wrap/TextWrap.py
@@ -0,0 +1,68 @@
+# Textwrap
+#
+# The textwrap module provides two convenient functions: wrap() and fill().
+#
+# textwrap.wrap()
+# The wrap() function wraps a single paragraph in text (a string) so that every line is width characters long at most.
+# It returns a list of output lines.
+#
+# >>> import textwrap
+# >>> string = "This is a very very very very very long string."
+# >>> print textwrap.wrap(string,8)
+# ['This is', 'a very', 'very', 'very', 'very', 'very', 'long', 'string.']
+# textwrap.fill()
+# The fill() function wraps a single paragraph in text and returns a single string containing the wrapped paragraph.
+#
+# >>> import textwrap
+# >>> string = "This is a very very very very very long string."
+# >>> print textwrap.fill(string,8)
+# This is
+# a very
+# very
+# very
+# very
+# very
+# long
+# string.
+#
+#
+# Task
+#
+# You are given a string S and width w.
+# Your task is to wrap the string into a paragraph of width w.
+#
+# Input Format
+#
+# The first line contains a string, S.
+# The second line contains the width, w.
+#
+# Constraints
+#
+# 0 < len(S) < 1000
+# 0 < w <len(S)
+#
+# Output Format
+#
+# Print the text wrapped paragraph.
+#
+# Sample Input
+#
+# ABCDEFGHIJKLIMNOQRSTUVWXYZ
+# 4
+#
+#
+# Sample Output
+#
+# ABCD
+# EFGH
+# IJKL
+# IMNO
+# QRST
+# UVWX
+# YZ
+
+
+def wrap(string, max_width):
+    return textwrap.fill(string,max_width)
+
+print(wrap(input(),int(input())))
